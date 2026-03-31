f09073c82265e30c1f2e0b7c4f39c1184bbff779
abhinaysindham <abhinay.sindham@gmail.com>
2026-03-31 21:14:59 +0530

    Day 1: Variables, data types, and f-strings exercise

---
+++ DAY01/variables.py
@@ -0,0 +1,14 @@
+name = "Alex"
+age = 25
+city = "Mumbai"
+experience = 2
+
+future_age = age + 10
+
+print(f"Hi, I'm {name}, {age} years old from {city}. In 10 years I'll be {future_age}.")
+
+print(type(name))
+print(type(age))
+print(type(city))
+print(type(experience))
+print(type(future_age))
